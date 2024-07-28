34b15e5ee7ec53ea3bb6216abb7f5bb73813614b
BrandonZS <abrandon34zs@gmail.com>
2024-07-28 00:51:40 -0600

    Se añadio estructura de frontend##Se añadio la estructura del frontend en MAUI

---
+++ PlanXFrontend/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PlanXFrontend
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
