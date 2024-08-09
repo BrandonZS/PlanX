85916e3ebbdf15d8039058d64e849d194f40456b
BrandonZS <abrandon34zs@gmail.com>
2024-08-08 18:57:57 -0600

    feat(Backend): Add logic struture for login API

---
+++ PlanX_BE/Entidades/Request/ReqLogin.cs
@@ -0,0 +1,16 @@
+﻿using PlanXBackend.Entidades.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanXBackend.Entidades.Request
+{
+    public class ReqLogin
+    {
+        public string email { get; set; }
+
+        public string password { get; set; }
+    }
+}
