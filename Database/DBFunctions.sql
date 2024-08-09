e742b0e8ca078465ccebf0086c364a28ee2cb655
BrandonZS <abrandon34zs@gmail.com>
2024-08-08 23:26:48 -0600

    Refactor(DB): Add comprobations and trycatch to the SP

--- Database/DBFunctions.sql
+++ Database/DBFunctions.sql
@@ -1,8 +1,8 @@
 
 --SP para registrar el usuario en la DB ////Ultima Actualizacion 8/8/2024 14:23
 Create OR Alter PROCEDURE SP_REGISTRO_USUARIO_REGULAR(
-    @NOMBRE nvarchar(50),
-    @APELLIDOS nvarchar(50),
+    @NOMBRE nvarchar(255),
+    @APELLIDOS nvarchar(255),
     @CORREO_ELECTRONICO nvarchar(max),
     @PASSWORD nvarchar(max),
     @FECNACIMIENTO DATETIME,
@@ -74,15 +74,15 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[sp_Login]
-    @CORREO_ELECTRONICO nVARCHAR(50), -- Tamaño típico para un email
-    @PASSWORD NVARCHAR(max),           -- Tamaño puede variar dependiendo de tu esquema de encriptación
+CREATE PROCEDURE SP_LOGIN
+    @CORREO_ELECTRONICO nVARCHAR(50), 
+    @PASSWORD NVARCHAR(max),           
     @id_usuario INT OUTPUT,
     @nombre NVARCHAR(50) OUTPUT,
     @apellidos NVARCHAR(50) OUTPUT
 AS
 BEGIN
-    SET @id_usuario = 0; -- Inicializar el valor en cero
+    SET @id_usuario = 0;
     SET @nombre = '';
     SET @apellidos = '';
     
@@ -100,68 +100,76 @@ BEGIN
     END
 END;
 
---SP para insertar un evento ////Ultima Actualizacion 8/8/2024 19:04
+--SP para insertar un evento ////Ultima Actualizacion 8/8/2024 20:46
 
-CREATE or alter PROCEDURE SP_CREAR_EVENTO
+CREATE or alter PROCEDURE SP_INSERTAR_EVENTO
 	@NOMBRE NVARCHAR(255),
 	@DESCRIPCION NVARCHAR(255),
 	@FECHORA_INICIO DATETIME,
 	@FECHORA_FIN DATETIME,
 	@LIMITE_USUARIO INT,
 	@DURACION FLOAT,
-	@EMAIL NVARCHAR(255)
+	@EMAIL NVARCHAR(255),
+	@IDRETURN int output,
+    @ERRORID int output,
+    @ERRORDESCRIPCION nvarchar(max) output
 AS
 BEGIN
 	
 	DECLARE @CodigoAlfanumerico NVARCHAR(6)
 	DECLARE @Existe BIT = 1
 
-	-- Ciclo DO WHILE
-	WHILE @Existe = 1
-	BEGIN
-
-		-- Generar el código alfanumérico
-		PRINT 'Este es un mensaje de prueba';
-		SET @CodigoAlfanumerico = SUBSTRING(CONVERT(VARCHAR(40), NEWID()), 1, 6)
-
-		-- Verificar si el código ya existe en la tabla Evento y si la fechaHoraFin ha pasado
-		IF NOT EXISTS (
-			SELECT 1 
-			FROM [dbo].[Evento] 
-			WHERE [codInvitacion] = @CodigoAlfanumerico 
-			AND [fechaHoraFin] > GETDATE()
-		)
+	BEGIN TRY
+		WHILE @Existe = 1
 		BEGIN
-			-- Si no existe o existe pero la fechaHoraFin ya ha pasado, salir del ciclo
-			SET @Existe = 0
+
+			SET @CodigoAlfanumerico = SUBSTRING(CONVERT(VARCHAR(40), NEWID()), 1, 6)
+
+			-- Verificar si el código ya existe en la tabla Evento y si la fechaHoraFin ha pasado
+			IF NOT EXISTS (
+				SELECT 1 
+				FROM [dbo].[Evento] 
+				WHERE [codInvitacion] = @CodigoAlfanumerico 
+				AND [fechaHoraFin] > GETDATE()
+			)
+			BEGIN
+				-- Si no existe o existe pero la fechaHoraFin ya ha pasado, salir del ciclo
+				SET @Existe = 0
+			END
 		END
-	END
 
-	DECLARE @ID_USER INT
-	SELECT @ID_USER =  [idUsuario] FROM [dbo].[Usuario] WHERE [email] = @EMAIL
+		DECLARE @ID_USER INT
+		SELECT @ID_USER =  [idUsuario] FROM [dbo].[Usuario] WHERE [email] = @EMAIL
 
-		        INSERT INTO [dbo].[Evento]
-                (
-                    [codInvitacion],
-					[nombre],
-					[descripcion],
-					[fechaHoraInicio],
-					[fechaHoraFin],
-					[limiteUsuarios],
-					[duracion],
-					[idUsuario]
-                )
-                VALUES
-                (
-                    @CodigoAlfanumerico,
-					@NOMBRE,
-					@DESCRIPCION,
-					@FECHORA_INICIO,
-					@FECHORA_FIN,
-					@LIMITE_USUARIO,
-					@DURACION,
-					@ID_USER
-                );
+			INSERT INTO [dbo].[Evento]
+			(
+				[codInvitacion],
+				[nombre],
+				[descripcion],
+				[fechaHoraInicio],
+				[fechaHoraFin],
+				[limiteUsuarios],
+				[duracion],
+				[idUsuario]	
+			)
+			VALUES
+			(
+				@CodigoAlfanumerico,
+				@NOMBRE,
+				@DESCRIPCION,
+				@FECHORA_INICIO,
+				@FECHORA_FIN,
+				@LIMITE_USUARIO,
+				@DURACION,
+				@ID_USER
+			);
+			SET @IDRETURN = SCOPE_IDENTITY();
+	END TRY
+	BEGIN CATCH
+        SET @IDRETURN = -1;
+        SET @ERRORID = ERROR_NUMBER();
+        SET @ERRORDESCRIPCION = ERROR_MESSAGE();
+    END CATCH
 END;
 
 
@@ -176,29 +184,38 @@ CREATE PROCEDURE SP_INSERTAR_TAREA
 	@PRIORIDAD VARCHAR(50)
 AS
 BEGIN
-	DECLARE @ID_USER INT
-	SELECT @ID_USER =  [idUsuario] FROM [dbo].[Usuario] WHERE [email] = @EMAIL
-	DECLARE @ID_PRIORIDAD INT
-	SELECT @ID_PRIORIDAD =  [idPrioridad] FROM [dbo].[Prioridad] WHERE [descripcion] = @PRIORIDAD
+	BEGIN TRY
+		DECLARE @ID_USER INT
+		SELECT @ID_USER =  [idUsuario] FROM [dbo].[Usuario] WHERE [email] = @EMAIL
 
-			        INSERT INTO [dbo].[Tarea]
-                (
-                    [titulo],
-					[descripcion],
-					[fechaHoraInicio],
-					[fechaHoraFinal],
-					[idUsuario],
-					[idPrioridad]
+		DECLARE @ID_PRIORIDAD INT
+		SELECT @ID_PRIORIDAD =  [idPrioridad] FROM [dbo].[Prioridad] WHERE [descripcion] = @PRIORIDAD
 
-                )
-                VALUES
-                (
-                    @TITULO,
-					@DESCRIPCION,
-					@FECHORA_INICIO,
-					@FECHORA_FIN,
-					@ID_USER,
-					@ID_PRIORIDAD
-                );
+			INSERT INTO [dbo].[Tarea]
+			(
+				[titulo],
+				[descripcion],
+				[fechaHoraInicio],
+				[fechaHoraFinal],
+				[idUsuario],
+				[idPrioridad]
 
+			)
+			VALUES
+			(
+				@TITULO,
+				@DESCRIPCION,
+				@FECHORA_INICIO,
+				@FECHORA_FIN,
+				@ID_USER,
+				@ID_PRIORIDAD
+			);
+			SET @IDRETURN = SCOPE_IDENTITY();    
+			
+	END TRY
+	BEGIN CATCH
+        SET @IDRETURN = -1;
+        SET @ERRORID = ERROR_NUMBER();
+        SET @ERRORDESCRIPCION = ERROR_MESSAGE();
+    END CATCH
 END
