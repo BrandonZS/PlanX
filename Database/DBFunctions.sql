981df90da7996d4649879c3b008274c79e6f9bae
BrandonZS <abrandon34zs@gmail.com>
2024-08-23 23:44:12 -0600

    fixsp

--- Database/DBFunctions.sql
+++ Database/DBFunctions.sql
@@ -698,29 +698,37 @@ BEGIN
 	END
 END
 CREATE OR ALTER PROCEDURE SP_DEFINIR_EVENTO
-	@FEC_INICIAL DATETIME,
-	@FEC_FINAL DATETIME,
-	@ID_USER INT,
-	@COD_INVI NVARCHAR (6),
-    @ERRORID int output,
-    @ERRORDESCRIPCION nvarchar(max) output
+    @FEC_INICIAL DATETIME,
+    @FEC_FINAL DATETIME,
+    @ID_USER INT,
+    @COD_INVI NVARCHAR(6),
+    @ERRORID INT OUTPUT,
+    @ERRORDESCRIPCION NVARCHAR(MAX) OUTPUT
 AS
 BEGIN
-	IF NOT EXISTS (SELECT 1 FROM [dbo].[Usuario] WHERE [idUsuario] = @ID_USER)
-	BEGIN
+    SET @ERRORID = 0; -- Inicializa el valor de ERRORID
+    
+    IF NOT EXISTS (SELECT 1 FROM [dbo].[Usuario] WHERE [idUsuario] = @ID_USER)
+    BEGIN
         SET @ERRORID = 1;
-		SET @ERRORDESCRIPCION = 'ERROR DESDE BD: USUARIO NO ENCONTRADO';
-	END
-	ELSE IF NOT EXISTS (SELECT 1 FROM [dbo].[Evento] WHERE [codInvitacion] = @COD_INVI)
-	BEGIN
-	    SET @ERRORID = 1;
-		SET @ERRORDESCRIPCION = 'ERROR DESDE BD: EVENTO NO ENCONTRADO';
-	END
-	ELSE 
-	BEGIN
-		UPDATE [dbo].[Evento]
-		SET [fechaHoraInicio] = @FEC_INICIAL, [fechaHoraFin] = @FEC_FINAL, [estado] = 1
-		WHERE [idUsuario] = @ID_USER AND [codInvitacion] = @COD_INVI AND [fechaHoraInicio] < GETDATE()
-	END
+        SET @ERRORDESCRIPCION = 'ERROR DESDE BD: USUARIO NO ENCONTRADO';
+    END
+    ELSE IF NOT EXISTS (SELECT 1 FROM [dbo].[Evento] WHERE [codInvitacion] = @COD_INVI)
+    BEGIN
+        SET @ERRORID = 1;
+        SET @ERRORDESCRIPCION = 'ERROR DESDE BD: EVENTO NO ENCONTRADO';
+    END
+    ELSE 
+    BEGIN
+        UPDATE [dbo].[Evento]
+        SET [fechaHoraInicio] = @FEC_INICIAL, [fechaHoraFin] = @FEC_FINAL, [estado] = 1
+        WHERE [idUsuario] = @ID_USER AND [codInvitacion] = @COD_INVI;
+        
+        IF @@ROWCOUNT = 0 -- Verifica si no se afectaron filas
+        BEGIN
+            SET @ERRORID = 1;
+            SET @ERRORDESCRIPCION = 'ERROR DESDE BD: EL UPDATE NO AFECTÓ NINGUNA FILA';
+        END
+    END
 END
 GO
