9b9814f3385e6b78968b26d625e0d2d58a93e647
BrandonZS <abrandon34zs@gmail.com>
2024-08-23 21:08:58 -0600

    Add directories

---
+++ PlanXFrontend/Entidades/Entities/ListaHorarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanXFrontend.Entidades.Entities
+{
+    public class ListaHorarios
+    {
+        public static List<Horario> listaHorarios = new List<Horario>();
+    }
+}
---
+++ PlanXFrontend/Entidades/Entities/Registro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanXFrontend.Entidades.Entities
+{
+    public class Registro
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fecInicial { get; set; }
+        public DateTime fecFinal { get; set; }
+    }
+}
